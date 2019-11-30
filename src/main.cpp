162f6c7a017be9b62b90c5075720d153b45a6950
Brian Erickson <berickson@gmail.com>
2019-11-29 19:57:53 -0800

    More bluetooth options

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,13 @@
 #endif
 #include <vector>
 
+#include <BluetoothSerial.h>
+#include <CmdParser.hpp>
+
+// globals
+const uint32_t bluetooth_buffer_reserve = 500;
+BluetoothSerial bluetooth;
+
 // *********************************
 // ESP32 Digital LED Driver
 //
@@ -17,6 +24,104 @@ const int STRANDCNT = sizeof(STRANDS)/sizeof(STRANDS[0]);;
 strand_t * strands[8];
 
 // **********************************
+
+
+
+
+
+//////////////////////////////////////////////////////
+// Command Interface
+
+class LineReader {
+  public:
+  const uint32_t buffer_size = 500;
+  String buffer;
+  String line;
+  //BluetoothSerial bluetoothx
+  bool line_available = true;
+
+  LineReader() {
+    buffer.reserve(buffer_size);
+    line.reserve(buffer_size);
+  }
+
+  bool get_line(Stream & stream) {
+    while(stream.available()) {
+      char c = (char)stream.read();
+      if(c=='\n') continue;
+      if(c== '\r') {
+        if(true){ //buffer.length() > 0) {
+          line = buffer;
+          buffer = "";
+          return true;
+        }
+      } else {
+        buffer.concat(c);
+      }
+    }
+    return false;
+  }
+  
+};
+
+class CommandEnvironment {
+public:
+  CommandEnvironment(CmdParser & args, Stream & cout, Stream & cerr) 
+  : args(args),cout(cout),cerr(cerr)
+  {
+  }
+  CmdParser & args;
+  Stream & cout;
+  Stream & cerr;
+  bool ok = true;
+};
+
+typedef void (*CommandCallback)(CommandEnvironment &env);
+
+
+class Command {
+public:
+  Command() 
+    : execute(nullptr)
+  {}
+  Command(const char * name, CommandCallback callback, const char * helpstring = nullptr) {
+    this->name = name;
+    this->execute = callback;
+    this->helpstring = helpstring;
+  }
+  String name;
+  CommandCallback execute;
+  String helpstring;
+};
+
+std::vector<Command> commands;
+
+void cmd_help(CommandEnvironment & env) {
+  for(auto command : commands) {
+    env.cout.print(command.name);
+    env.cout.print(": ");
+    env.cout.print(command.helpstring);
+    env.cout.println();
+  }
+}
+
+enum LightMode {
+  mode_rgb,
+  mode_usa,
+  mode_explosion,
+  mode_pattern1,
+  mode_rainbow,
+  mode_green,
+  mode_color,
+  mode_off // mode off must always be last
+};
+
+LightMode light_mode = mode_rainbow;
+
+
+
+
+
 
 
 // Neopixel constants
@@ -54,9 +159,104 @@ namespace Colors {
     uint32_t blue = strip.Color(0,0,20);
 };
 
+
+void cmd_rgb(CommandEnvironment & env) {
+  light_mode = mode_rgb;
+}
+
+void cmd_usa(CommandEnvironment & env) {
+  light_mode = mode_usa;
+}
+
+void cmd_explosion(CommandEnvironment & env) {
+  light_mode = mode_explosion;
+}
+
+void cmd_pattern1(CommandEnvironment & env) {
+  light_mode = mode_pattern1;
+}
+
+void cmd_rainbow(CommandEnvironment & env) {
+  light_mode = mode_rainbow;
+}
+
+void cmd_off(CommandEnvironment & env) {
+  light_mode = mode_off;
+}
+
+void cmd_green(CommandEnvironment & env) {
+  light_mode = mode_green;
+}
+
+
+std::vector<uint32_t> current_colors = {strip.Color(15,15,15)};
+
+void cmd_color(CommandEnvironment & env) {
+  light_mode = mode_color;
+  uint32_t color = strip.Color(atoi(env.args.getCmdParam(1)),atoi(env.args.getCmdParam(2)),atoi(env.args.getCmdParam(3)));
+  current_colors = {color};
+}
+
+void cmd_add_color(CommandEnvironment & env) {
+  light_mode = mode_color;
+  uint32_t color = strip.Color(atoi(env.args.getCmdParam(1)),atoi(env.args.getCmdParam(2)),atoi(env.args.getCmdParam(3)));
+  current_colors.push_back(color);
+}
+
+
+
+void cmd_next(CommandEnvironment & env) {
+  light_mode = (LightMode) (light_mode + 1);
+  if(light_mode > mode_off) {
+    light_mode = (LightMode) 0;
+  }
+}
+
+void cmd_previous(CommandEnvironment & env) {
+  if(light_mode==0) {
+    light_mode = mode_off;
+  } else {
+    light_mode = (LightMode)(light_mode - 1);
+  }
+
+}
+
+
+
+Command * get_command_by_name(const char * command_name) {
+  for(auto & command : commands) {
+    if(command.name == command_name) {
+      return &command;
+    }
+  }
+  return nullptr;
+}
+
+
 using namespace Colors;
 
 void setup() {
+  Serial.begin(921600);
+  bluetooth.begin("bke");
+  // 
+  // set up command interfaces
+  commands.reserve(50);
+  commands.emplace_back(Command{"help", cmd_help, "displays list of available commands"});
+  commands.emplace_back(Command{"usa", cmd_usa, "red white and blue"});
+  commands.emplace_back(Command{"rgb", cmd_rgb, "red green and blue lights"});
+  commands.emplace_back(Command{"explosion", cmd_explosion, "colored explosions"});
+  commands.emplace_back(Command{"pattern1", cmd_pattern1, "lights chase at different rates"});
+  commands.emplace_back(Command{"rainbow", cmd_rainbow, "rainbow road"});
+  commands.emplace_back(Command{"green", cmd_green, "solid green color"});
+  commands.emplace_back(Command("color", cmd_color, "solid {red} {blue} {green}"));
+  commands.emplace_back(Command("add", cmd_add_color, "adds a color to current pallet {red} {blue} {green}"));
+
+  commands.emplace_back(Command{"next", cmd_next, "cycles to the next mode"});
+  commands.emplace_back(Command{"previous", cmd_previous, "cycles to the previous mode"});
+
+  commands.emplace_back(Command{"off", cmd_off, "lights turn off, device is still running"});
+
+
   // setup the LED strand
   //strip.begin();
   //strip.show(); // Initialize all pixels to 'off'
@@ -127,6 +327,10 @@ void rainbow(int pixels_per_second = 30) {
   strip.show();
 }
 
+void off() {
+  strip.clear();
+}
+
 void explosion() {
   uint32_t ms = millis();
   static int16_t center_led=0;
@@ -173,8 +377,65 @@ void usa() {
 };
 
 
+bool every_n_ms(unsigned long last_loop_ms, unsigned long loop_ms, unsigned long ms) {
+  return (last_loop_ms % ms) + (loop_ms - last_loop_ms) >= ms;
+}
 
 void loop() {
+  static unsigned long last_loop_ms = 0;
+  unsigned long loop_ms = millis();
+
+  if(every_n_ms(last_loop_ms, loop_ms, 1000)) {
+    Serial.print("loop ");
+    Serial.println(loop_ms);
+  }
+  
+  static LineReader line_reader;
+  static String last_bluetooth_line;
+
+  // check for a new line in bluetooth
+  if(line_reader.get_line(bluetooth)) {
+    CmdParser parser;
+    parser.parseCmd((char *)line_reader.line.c_str());
+    Command * command = get_command_by_name(parser.getCommand());
+    if(command) {
+      CommandEnvironment env(parser, bluetooth, bluetooth);
+      command->execute(env);
+    } else {
+      bluetooth.print("ERROR: Command not found - ");
+      bluetooth.println(parser.getCommand());
+    }
+    last_bluetooth_line = line_reader.line;
+  }
+
+  switch(light_mode) {
+    case mode_usa:
+      usa();
+      break;
+    case mode_rainbow:
+      rainbow();
+      break;
+    case mode_rgb:
+      rgb();
+      break;
+    case mode_green:
+      repeat({strip.Color(0,50,0)});
+      break;
+    case mode_explosion:
+      explosion();
+      break;
+    case mode_pattern1:
+      pattern1();
+      break;
+    case mode_color:
+      repeat(current_colors);
+      break;
+
+    case mode_off:
+      off();
+      break;
+  }
+  
   //trans();
   //rainbow();
   //usa();
@@ -185,7 +446,7 @@ void loop() {
   //repeat({strip.Color(20,10,0), strip.Color(25,16,0),strip.Color(14,5,2)});// orange/brown?
   //repeat({strip.Color(20,0,0), strip.Color(0,20,0)});// red/green 0.063A
   //repeat({strip.Color(255,255,255)}); // bright white, 2.123A / 50LEDS
-  explosion();
+  // explosion();
 
   for (uint16_t i = 0; i < strip.numPixels(); i++) {
     auto & p = strands[0]->pixels[i];
@@ -195,9 +456,13 @@ void loop() {
     
   }
 
+
   digitalLeds_drawPixels(strands, STRANDCNT);
-  esp_sleep_enable_timer_wakeup(30000);
-  esp_light_sleep_start();
+  
+  delay(light_mode == mode_off ? 2000 : 20);
+  //esp_sleep_enable_timer_wakeup(30000);
+  //esp_light_sleep_start();
+  last_loop_ms = loop_ms;
  }
 
  
